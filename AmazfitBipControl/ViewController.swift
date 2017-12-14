022eb51eb902c7a996ce50e7bc38d1570c5b6293
krutskih_ov <krutskih_ov@magnit.ru>
2017-12-14 13:30:15 +0300

    Initial Commit

---
+++ AmazfitBipControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AmazfitBipControl
+//
+//  Created by Круцких Олег on 14.12.2017.
+//  Copyright © 2017 Круцких Олег. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
