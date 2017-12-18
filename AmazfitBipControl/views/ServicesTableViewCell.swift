0577d2ce23c9efb4dae294e9f627b2ae092fc3e5
krutskih_ov <krutskih_ov@magnit.ru>
2017-12-18 20:16:33 +0300

    services to DB + sync

---
+++ AmazfitBipControl/views/ServicesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ServicesTableViewCell.swift
+//  AmazfitBipControl
+//
+//  Created by Круцких Олег on 18.12.2017.
+//  Copyright © 2017 Круцких Олег. All rights reserved.
+//
+
+import UIKit
+
+class ServicesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
